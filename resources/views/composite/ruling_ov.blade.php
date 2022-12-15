737c4ae40121c837d9e9262ffd057eabc1814672
tenkabutu <tenkabutu100@gmailcom>
2022-12-15 15:21:06 +0900

    Revert "no message"
    This reverts commit 04a8d6349537f7f90eebe8e02bfc3f83f44405b1.

--- resources/views/composite/ruling_ov.blade.php
+++ resources/views/composite/ruling_ov.blade.php
@@ -16,10 +16,11 @@
                 <legend>検索条件</legend>
     		<form action="" method="GET">
     		@csrf
-    		<input type="hidden" name="matter_type" value="1">
+    		<!-- <input type="hidden" name="matter_type" value="1"> -->
     		<ul>
 
-    			<li><div>日時：<select id="year" name="year">
+    			<li>
+    			<div>日時：<select id="year" name="year">
     				<option value="2022">2022</option><option value="2021">2021</option></select>年
 					<select id="month" name="month">
 						<option value="0">--</option>
@@ -27,6 +28,17 @@
 							<option value={{$i}} @if(isset($input_data['month'])&&$i==$input_data['month']) selected @endif>{{$i}}</option>
 						@endfor
 						</select>月
+					</div>
+						<div><label>状態：</label>
+					 <div class="radio-group">
+					<input id="st1_1" type="radio" class="st1" name="matter_type" value="1" @if(Request::get('matter_type')==1) checked @endif />
+					<label for="st1_1">振替</label>
+					<input id="st1_2" type="radio" class="st1" name="matter_type" value="2" @if(Request::get('matter_type')==2) checked @endif/>
+					<label for="st1_2">休暇</label>
+					<input id="st1_3" type="radio" class="st1" name="matter_type" value="3" @if(Request::get('matter_type')==3) checked @endif/>
+					<label for="st1_3">テレワ</label>
+
+					</div>
 					</div>
 					<div><label>状態：</label>
 					 <div class="radio-group">
@@ -51,84 +63,9 @@
     	</form>
     	</fieldset>
     </div>
-    <div class="">
-		<table class="task_table sort-table">
-			<thead>
-			<tr>
-				<th class="id" sortable>No</th>
-				<th>申請者</th>
-				<th class="id">申請日</th>
-				<th class="id">実施日</th>
-				<th class="id">開始時間</th>
-				<th class="id">終了時間</th>
-				<th class="s3">時間</th>
-				<th>案件状態</th>
-				<th class="id">申請/確認日</th>
-				<th class="auto">作業</th>
-				<th></th>
-
-			</tr>
-			</thead>
-			@if(isset($records))
-			{{-- @foreach ($records as $record) --}}
-			@php
-			 $back = 0;
-			@endphp
-			@foreach ($records as $id =>$record)
-			@if($back!=$record->matters_id)
-			<tr class="d{{$id+1}}">
-
-
-				<td><a href="/shinsei2/public/{{ $record->matters_id }}/rewrite_ov">{{ $record->matters_id}}</a></td>
-				<td>{{ $record->username}}</td>
-				<td>{{ date('n/j',strtotime($record->matter_request_date))}}</td>
-				<td>{{ date('n/j',strtotime($record->matter_change_date))}}</td>
-				<td>{{$record->hour1.":".$record->minutes1}}</td>
-				<td>{{$record->hour2.":".$record->minutes2}}</td>
-
-				<td>{{floor($record->allotted/60).'時間 '.($record->allotted%60).'分'}}</td>
-				<td>{{ $record->statusname}}</td>
-				<td>@if($record->status==2){{date('n/j',strtotime($record->matter_request_date))}}@elseif($record->status>=3){{date('n/j',strtotime($record->matter_reply_date))}}@endif</td>
-				<td>{{$record->allotted}}</td>
-				<td><button class="show_ov" onclick="location.href='/shinsei2/public/{{ $record->matters_id }}/rewrite_ov'">詳細</button></td>
-				</tr>
-
-				@endif
-				@php
-					if($back!=$record->matters_id){
-						$back =$record->matters_id;
-					};
-				@endphp
-				@if($record->matter_id)
-				<tr>
-				<td></td>
-				<td></td>
-				<td>⇒</td>
-				<td>{{date('n/j',strtotime($record->task_change_date))}}</td>
-				<td>{{$record->task_hour1.":".$record->task_minutes1}}</td>
-				<td>{{$record->task_hour2.":".$record->task_minutes2}}</td>
-				<td>{{floor($record->task_allotted/60).'時間 '.($record->task_allotted%60).'分'}}</td>
-				@if($record->task_status==2)
-				<td>申請中</td><td>{{date('n/j',strtotime($record->task_request_date))}}</td>
-				@elseif($record->task_status>=3)
-				<td>確認済み</td><td>{{date('n/j',strtotime($record->task_reply_date))}}</td>
-				@else
-				<td></td><td></td>
-				@endif
-				<td>{{$record->task_total}}</td>
-				<td></td>
-				@endif
-
-
-
-			</tr>
-
-			@endforeach
-			@endif
-
-
-		</table>
-	</div>
+    <!-- @isset($records) -->
+    	<x-ruling-box :role="Auth::user()->role" type="振替" :records="$records"/>
+	<!-- @endisset -->
 </div>
 <script>
 $(function(){
@@ -150,35 +87,7 @@ $('label', radio).click(function() {
     //	alert($('input[name="index50"]:checked').val());
     //    }
 });
-$('input[name="index50"]').change(function() {
-	var val1 = $('input[name="index50"]:checked').val();
-	var val2= $('input[name="schooltype"]:checked').val();
 
-
-	if(val2=="小学校"||val2=="中学校"){
-
-		$('select[name="index_select"]').find('option').each(function() {
-      var val3 = $(this).attr("class");
-      var val4 = $(this).data("val");
-      if (val1 == val3&&val2==val4) {
-        $(this).show();
-      }else{
-        $(this).hide();
-      }
-    	});
-	}else{
-		$('select[name="index_select"]').find('option').each(function() {
-	          var val3 = $(this).attr("class");
-	          var val4 = $(this).data("val");
-	          if (val1 == val3) {
-	            $(this).show();
-	          }else{
-	            $(this).hide();
-	          }
-	        	});
-
-    	}
-});
 
 
 });
