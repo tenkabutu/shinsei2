34f4a55d8126cb2cd442fedfaa8eb5d8152e4ad2
tenkabutu <tenkabutu100@gmailcom>
2022-12-15 15:19:18 +0900

    no message

--- app/View/Components/RulingBox.php
+++ app/View/Components/RulingBox.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class rulingBox extends Component
+class RulingBox extends Component
 {
     /**
      * Create a new component instance.
