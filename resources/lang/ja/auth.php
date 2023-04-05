469f43d0678ca27f9f72b42fd7c3d58fdbe1f6a8
tenkabutu <tenkabutu100@gmailcom>
2023-04-05 13:35:24 +0900

    no message

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '登録メールアドレスか、パスワードが間違っています。パスワード変更機能が未実装なので見に覚えがなければ管理者まで連絡してください。',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
