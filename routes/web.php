cdb81811e5bb3f8c91b0a656a368a074ccd44666
tenkabutu <tenkabutu100@gmailcom>
2023-04-14 12:07:58 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,7 @@ Route::post('{id}/delete_pa',[PaidLeaveController::class,'delete']);
 
 Route::post('{id}/accept',[AcceptController::class,'accept']);
 Route::post('{id}/reject',[AcceptController::class,'reject']);
+Route::post('{id}/cancel',[AcceptController::class,'cancel']);
 
 Route::get('matter_search',[ShinseiController::class,'matter_search']);
 Route::get('{type}/matter_search',[ShinseiController::class,'matter_search2']);
