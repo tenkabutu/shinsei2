b1d257996d24ee8a04b6a4913e0e8995d9270feb
tenkabutu <tenkabutu100@gmailcom>
2023-04-28 15:02:53 +0900

    no message

--- resources/views/components/rulingbox/ruling-box-pa.blade.php
+++ resources/views/components/rulingbox/ruling-box-pa.blade.php
@@ -3,8 +3,9 @@
 			<thead>
 			<tr>
 				<th class="id" sortable>ID</th>
-				<th>申請者</th>
 				<th>種類</th>
+				<th>UID</th>
+				<th>申請者</th>
 				<th class="id">申請日</th>
 				<th class="id">実施日</th>
 				<th class="id">開始時刻</th>
@@ -25,7 +26,9 @@
 
 				<td>{{ $record->matters_id}}</td>
 				<td>{{ $record->optname}}</td>
+				<td>{{ $record->user_id}}</td>
 				<td>{{ $record->username}}</td>
+
 				<td>{{ date('n/j',strtotime($record->matter_request_date))}}</td>
 				<td>{{ date('n/j',strtotime($record->matter_change_date))}}</td>
 				<td>{{$record->hour1."時".$record->minutes1}}分</td>
