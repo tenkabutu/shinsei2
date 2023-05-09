8c0a6fd934d4a58e779cc94743c2cdd653b107bc
tenkabutu <tenkabutu100@gmailcom>
2023-05-09 16:03:14 +0900

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-				<link rel="icon" type="image/vnd.microsoft.icon" href="/tabusapo_lb/public/img/favicon.ico">
+				<link rel="icon" type="image/vnd.microsoft.icon" href="/shinsei2/public/img/favicon.ico">
 				<title>{{ config('app.name', 'Laravel2') }}</title>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
