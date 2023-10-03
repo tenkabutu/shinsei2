0abdd1b2d67ca8fee6f8416fa759fc7c86d35b6b
tenkabutu <tenkabutu100@gmailcom>
2023-10-03 10:48:56 +0900

    no message

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -142,7 +142,6 @@
 
 						<li class="nav_count"><a href="/shinsei2/public/7/matter_search">申請一覧</a>　<a class="double2">要修正</a>@else　　　 </li>
 
-
 				</ul>
 					<div class='side_label'>
 					<span>貸出申請</span>
