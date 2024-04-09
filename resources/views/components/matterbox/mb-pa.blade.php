0c33a530c519857c8514a1ecbffd091ca0fa27dd
tenkabutu <tenkabutu100@gmailcom>
2024-04-09 16:34:04 +0900

    no message

--- resources/views/components/matterbox/mb-pa.blade.php
+++ resources/views/components/matterbox/mb-pa.blade.php
@@ -8,7 +8,7 @@
 		<div>
 			<label class="g12">種別1:</label>
 			<div class="radio-group g23">
-				<!--<input id="st1_1" type="radio" class="st1" name="opt1" value="1" {{old('opt1')=='1' ? 'checked':''}} />
+				<input id="st1_1" type="radio" class="st1" name="opt1" value="1" {{old('opt1')=='1' ? 'checked':''}} />
 				<label id="st1_1_label" for="st1_1" style="{{ (!isset($residue_rest_day)  || $residue_rest_day < 1) ? 'background-color: red;' : '' }}">全日</label>
 				<input id="st1_2" type="radio" class="st1" name="opt1" value="2" {{old('opt1')=='2' ? 'checked':''}}/>
 				<label id="st1_2_label" for="st1_2" style="{{ (!isset($residue_rest_day)  || $residue_rest_day ==0) ? 'background-color: red;' : '' }}">午前休</label>
@@ -16,7 +16,8 @@
 				<label id="st1_3_label" for="st1_3" style="{{ (!isset($residue_rest_day)  || $residue_rest_day ==0) ? 'background-color: red;' : '' }}">午後休</label>
 				<input id="st1_4" type="radio" class="st1" name="opt1" value="4" {{old('opt1')=='4' ? 'checked':''}}/>
 				<label id="st1_4_label" for="st1_4">時間休</label>
-				-->
+
+				<!--
 				<input id="st1_1" type="radio" class="st1" name="opt1" value="1" {{old('opt1')=='1' ? 'checked':''}} />
 				<label for="st1_1">全日</label>
 				<input id="st1_2" type="radio" class="st1" name="opt1" value="2" {{old('opt1')=='2' ? 'checked':''}}/>
@@ -25,6 +26,7 @@
 				<label for="st1_3">午後休</label>
 				<input id="st1_4" type="radio" class="st1" name="opt1" value="4" {{old('opt1')=='4' ? 'checked':''}}/>
 				<label for="st1_4">時間休</label>
+				-->
 
 			</div>
 			<label class="g34">種別2:</label>
