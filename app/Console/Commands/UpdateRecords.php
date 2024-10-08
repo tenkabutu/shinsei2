198c1780a7d890c957ba5ac96187c72b4eb4c188
tenkabutu <tenkabutu100@gmailcom>
2024-10-08 15:53:23 +0900

    no message

--- app/Console/Commands/UpdateRecords.php
+++ app/Console/Commands/UpdateRecords.php
@@ -38,7 +38,7 @@ class UpdateRecords extends Command
      */
     public function handle()
     {
-        matter::where('matter_type',8)->update(['hour1' => '1']);
+        Matter::where('matter_type',8)->update(['hour1' => '1']);
         $this->info('Records updated successfully.');
     }
 }
