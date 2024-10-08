9b0afbe6639ac9674ddaffbcef1bd460b159e213
tenkabutu <tenkabutu100@gmailcom>
2024-10-08 15:40:00 +0900

    no message

---
+++ app/Console/Commands/UpdateRecords.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Matter;
+
+class UpdateRecords extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:records';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update specific records in the table daily at midnight';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        matter::where('matter_type',8)->update(['hour1' => '1']);
+        $this->info('Records updated successfully.');
+    }
+}
