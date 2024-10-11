87d5b35cb0f604e5f83748d55bcafdedc241f9c2
tenkabutu <tenkabutu100@gmailcom>
2024-10-11 10:28:45 +0900

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('update:records')->dailyAt('10:22');
+        $schedule->command('update:records')->dailyAt('10:30');
     }
 
     /**
